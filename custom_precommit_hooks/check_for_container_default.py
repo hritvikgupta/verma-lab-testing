f7ffd1267e98c6c73c1675e7d3263918a81478af
Hritvik Gupta <60143996+hritvikgupta@users.noreply.github.com>
2024-08-12 17:29:24 -0400

    Update check_for_container_default.py

--- custom_precommit_hooks/check_for_container_default.py
+++ custom_precommit_hooks/check_for_container_default.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import argparse
 from typing import Sequence
 import logging
-from custom_precommit_hooks.utils import added_files
+from utils import added_files
 
 container_definers = [
     'process.container = ',
